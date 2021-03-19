1672894260a04fc77185b46bff9720a863e2eb0a
Bruno Lovric <brunoolovric@gmail.com>
2021-03-19 23:24:09 +0100

    added nested objects project

---
+++ ex06/src/index.js
@@ -0,0 +1,16 @@
+function myNes() {
+    var myStorage = {
+        "car": {
+            "inside": {
+                "glove box": "maps",
+                "passenger seat": "crumbs"
+            },
+            "outside": {
+                "trunk": "jack"
+            }
+        }
+    };
+
+}
+console.log(myNes());
+module.exports = myNes;

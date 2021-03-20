cf5929ec4fb223b9a299757f28dbad00fa09b3ab
Bruno Lovric <brunoolovric@gmail.com>
2021-03-20 09:35:06 +0100

    added range project

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,5 +1,16 @@
 function rangeOfNumbers(startN, endN) {
-
-}
-console.log(rangeOfNumbers());
+    if (endN - startN === 0) {
+        return [startN];
+    } else if (endN - startN < 0) {
+        return "The starting number will always be less than or equal to the ending number";
+    } else {
+        var list = rangeOfNumbers(startN +1, endN);
+        list.unshift(startN);
+        return list;
+    }
+}        
+console.log(rangeOfNumbers(1, 7));
+console.log(rangeOfNumbers(3, 10));
+console.log(rangeOfNumbers(5, 5));
+console.log(rangeOfNumbers(7, 5));
 module.exports = rangeOfNumbers;

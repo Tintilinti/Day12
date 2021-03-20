fe3a17fab40e6d337b01bbd25b542f18e4f6a869
Bruno Lovric <brunoolovric@gmail.com>
2021-03-20 09:55:04 +0100

    added sumFibonacci project

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,5 +1,19 @@
 function sumFibonacci(num) {
-
+    var number = 0;
+    var nextNumber = 1;
+    var sum = 0;
+    while (nextNumber <= num) {
+        if ( nextNumber % 2 !== 0) {
+            sum += nextNumber;
+        }
+        nextNumber += number;
+        number = nextNumber - number;
+    }
+    return sum;
 }
-console.log(sumFibonacci(num));
+console.log(sumFibonacci(1));
+console.log(sumFibonacci(10));
+console.log(sumFibonacci(20));
+console.log(sumFibonacci(4));
+console.log(sumFibonacci(-5));
 module.exports = sumFibonacci;
